d6a84562861c2e18719f1fce9a0ec41727d08c06
Nguyen Viet Tuang <tuan.nguyenviet271@gmail.com>
2021-08-29 15:51:49 +0700

    No longer need part 3

--- part_3/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root "todos#index"
-
-  resources :todos, only: %i[index new create] do
-    resource :completion, only: %i[create destroy]
-  end
-
-  resource :session, only: %i[new create]
-end
