cbad9fa532a5aafbafbc7aac02bdc150305ad9eb
Nguyen Viet Tuang <tuan.nguyenviet271@gmail.com>
2021-08-29 15:55:13 +0700

    Adjust for testing

--- part_1/solution.py
+++ part_1/solution.py
@@ -5,7 +5,7 @@ class Solution:
         n = len(arr)
         l, r = 0, n - 1
         while l <= r:
-            if l == r or l == r + 1:
+            if l == r or l == r - 1:
                 return max(arr[l], arr[r])
             m = (l + r) // 2
             if arr[m - 1] < arr[m] and arr[m] > arr[m + 1]:
